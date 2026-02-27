a3560422d5bbc9284d05d2241dd1179086c911e1
Manikanta Thota <manikantathota1013@gmail.com>
2026-02-27 14:29:50 +0530

    Update LoginServlet.java

--- src/main/java/com/example/LoginServlet.java
+++ src/main/java/com/example/LoginServlet.java
@@ -1,12 +1,19 @@
 package com.example;
 
 import java.io.IOException;
+import java.sql.*;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+    private static final String DB_URL =
+            "jdbc:mysql://localhost:3306/whatsapp_db";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "root"; // change password
 
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
@@ -17,10 +24,30 @@ public class LoginServlet extends HttpServlet {
 
         response.setContentType("text/html");
 
-        if (phone.equals("admin") && password.equals("1234")) {
-            response.getWriter().println("<h2>Login Successful ✅</h2>");
-        } else {
-            response.getWriter().println("<h2>Invalid Credentials ❌</h2>");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.getConnection(
+                    DB_URL, DB_USER, DB_PASS);
+
+            String sql = "SELECT * FROM users WHERE phone=? AND password=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, phone);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                response.getWriter().println("<h2>Login Successful ✅</h2>");
+            } else {
+                response.getWriter().println("<h2>Invalid Credentials ❌</h2>");
+            }
+
+            conn.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("<h3>Database Error ❌</h3>");
         }
     }
 }
